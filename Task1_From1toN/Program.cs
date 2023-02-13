d453bfa936f88a4a1771324a638fd27188b0cde6
Roman_Anatshev <romananatshev@gmail.com>
2023-02-13 11:05:51 +0300

    Показать натуральные числа от N до 1, N задано

---
+++ Task1_From1toN/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Показать натуральные числа от N до 1, N задано
+
+
+void ShowMe (int N)
+{
+    if (N==0) return ;
+    Console.WriteLine(N);
+    ShowMe (N-1);
+
+}
+
+Console.WriteLine("enter any integer");
+int N = int.Parse(Console.ReadLine()!);
+ShowMe(N);
+
